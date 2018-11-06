cd679e350c2a6969069ee166a35b0cbbc2594ea3
Henrique de Almeida <henriquealmeida1492@gmail.com>
2018-11-06 11:38:00 -0200

    Trata adequação de altura de parent view de para com lista de alimentos

--- app/src/main/java/com/example/henriqueribeirodealmeida/nutre/newMealActivity.java
+++ app/src/main/java/com/example/henriqueribeirodealmeida/nutre/newMealActivity.java
@@ -3,9 +3,13 @@ package com.example.henriqueribeirodealmeida.nutre;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.ScrollView;
 
 import com.example.henriqueribeirodealmeida.nutre.Adapters.AddedFoodAdapter;
 import com.example.henriqueribeirodealmeida.nutre.Entities.Food;
@@ -23,6 +27,7 @@ public class newMealActivity extends AppCompatActivity {
         ImageView addMeal = findViewById(R.id.add_meal);
         Button addItemButton = findViewById(R.id.add_item);
         ListView addedFoodList = findViewById(R.id.added_food_list);
+        ScrollView mainScrollView = findViewById(R.id.main_scroll_view);
 
         //temp
         ArrayList<Food> foods = new ArrayList<>();
@@ -33,5 +38,32 @@ public class newMealActivity extends AppCompatActivity {
 
         AddedFoodAdapter adapter = new AddedFoodAdapter(this, foods);
         addedFoodList.setAdapter(adapter);
+        setListViewHeight(addedFoodList);
+    }
+
+    private void setListViewHeight(ListView listView) {
+
+        ListAdapter adapter = listView.getAdapter();
+        int totalHeight = 0;
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.EXACTLY);
+
+        for (int i = 0; i < adapter.getCount(); i++) {
+
+            View groupItem = adapter.getView(i, null, listView);
+            groupItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+
+            totalHeight += groupItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        int height = totalHeight + (listView.getDividerHeight() * (adapter.getCount() - 1));
+
+        if (height < 10){
+            height = 200;
+        }
+
+        params.height = height;
+        listView.setLayoutParams(params);
+        listView.requestLayout();
     }
 }
